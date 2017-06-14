82c91c3f69e59f590513e5a5865c2f59e3ad85df
Noam Silvy <noam.silvy@epitech.eu>
2017-06-14 15:58:53 +0200

    Add init function in main.src

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,22 @@
 #include "FeedFactory.hpp"
 #include "registerAllContexts.hpp"
 
+void init(irr::IrrlichtDevice *device)
+{
+  indie::engine::provideDevice(device);
+  indie::system::feedFactory();
+  indie::context::registerAllContexts();
+}
+
 int main()
 {
   irr::IrrlichtDevice *device =
     irr::createDevice(irr::video::EDT_OPENGL,
 		      irr::core::dimension2d<irr::u32>(1920, 968), 16, false);
-  indie::engine::provideDevice(device);
-  indie::system::feedFactory();
-  indie::context::registerAllContexts();
+  init(device);
   // CODE ABOVE ABSOLUTLY NECESSARY BEFORE ANYTHING ELSE
 
-  // ecs::Context			ctx = {{indie::system::INPUT,
-  // 							true}};
   indie::engine::contextManager().push(indie::context::MAIN_MENU);
-  // indie::engine::contextManager().push(&ctx);
 
   auto id = indie::engine::entityManager().createEntity();
   indie::engine::eventManager().subscribe(42, [](ecs::Entity id){
