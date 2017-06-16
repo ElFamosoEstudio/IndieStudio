5be05004f81ca6ded7bf278560d5d0fa8ff5db6d
Noam Silvy <noam.silvy@epitech.eu>
2017-06-17 00:33:11 +0200

    Add indie::event namespace

--- src/indie/core/indie.hpp
+++ src/indie/core/indie.hpp
@@ -5,7 +5,7 @@
 // Login   <silvy_n@epitech.net>
 //
 // Started on  Fri Jun 16 04:08:13 2017 Noam Silvy
-// Last update Fri Jun 16 04:17:53 2017 Noam Silvy
+// Last update Sat Jun 17 00:32:06 2017 Noam Silvy
 //
 
 #ifndef INDIE_HPP
@@ -16,6 +16,7 @@ namespace indie
   void		init();
 }
 
+# include "event.hpp"
 # include "system.hpp"
 # include "context.hpp"
 # include "entity.hpp"
---
+++ src/indie/events/event.hpp
@@ -0,0 +1,26 @@
+//
+// event.hpp for indie studio in /home/silvy_n/projects/IndieStudio/src/indie/events
+//
+// Made by Noam Silvy
+// Login   <silvy_n@epitech.net>
+//
+// Started on  Fri Jun 16 21:09:13 2017 Noam Silvy
+// Last update Fri Jun 16 22:37:52 2017 Noam Silvy
+//
+
+#ifndef EVENT_HPP
+# define EVENT_HPP
+
+namespace indie
+{
+  namespace event
+  {
+    enum EEvent
+      {
+	// BOMB_EXPLODED -> event happened
+	// GO_RIGHT -> action requested
+      };
+  }
+}
+
+#endif //!EVENT_HPP
