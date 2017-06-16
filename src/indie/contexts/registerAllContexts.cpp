5401b5131dfbea918546549ace68bb3796c58f2c
Adam Akkari <adam.akkari@epitech.eu>
2017-06-16 23:45:34 +0200

    Contexte game register

--- src/indie/contexts/registerAllContexts.cpp
+++ src/indie/contexts/registerAllContexts.cpp
@@ -5,7 +5,7 @@
 // Login   <silvy_n@epitech.net>
 //
 // Started on  Wed Jun 14 03:59:36 2017 Noam Silvy
-// Last update Fri Jun 16 21:42:32 2017 akram abd-ali
+// Last update Fri Jun 16 23:42:46 2017 Adam Akkari
 //
 
 #include "ecs.hpp"
@@ -16,4 +16,9 @@
 void		indie::context::registerAllContexts()
 {
   indie::engine::contextManager().registerContext(MAIN_MENU, {{indie::system::COLLISION, true}});
+  indie::engine::contextManager().registerContext(GAME,{
+      {indie::system::COLLISION, true},
+	{indie::system::MESH_RENDERER, true},
+      {indie::system::CAMERA_SYSTEM, true}});
+  //TODO: trouver comment indenter ça proprement
 }
