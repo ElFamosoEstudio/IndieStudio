c581e65f50efb3adbc0ba8525552ba07a1301360
Bento <mohamed.bentorcha@epitech.eu>
2017-06-17 05:32:09 +0200

    add menu v1 + img

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
 #include "irrlicht.h"
 #include "indie.hpp"
+#include "Menu.hpp"
 
-int	main()
+int	main(void)
 {
   indie::system::Collision col;
   indie::system::MeshRenderer col2;
   indie::init(); // For now: registers all factories
+  indie::MainMenu		manu;
 
   indie::engine::contextManager().push(indie::context::GAME);
 
   while (indie::gfx::device()->run())
       {
 	indie::engine::inputReceiver().disableInputHandling();
-	indie::gfx::videoDriver()->beginScene(true, true,
-						  irr::video::SColor(255, 255, 255, 255));
+	indie::gfx::videoDriver()->beginScene(true, true, irr::video::SColor(255, 255, 255, 255));
+	manu.update();
 	indie::engine::systemManager().update();
 	indie::gfx::sceneManager()->drawAll();
 	indie::gfx::videoDriver()->endScene();
-        indie::engine::inputReceiver().enableInputHandling();	
+        indie::engine::inputReceiver().enableInputHandling();
       }
   indie::engine::inputReceiver().disableInputHandling();
   indie::gfx::device()->drop();
