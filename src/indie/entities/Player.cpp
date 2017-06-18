2cbe3106a6bdf005efd70a5dbb79adfc15fea43a
Adam Akkari <adam.akkari@epitech.eu>
2017-06-18 13:03:15 +0200

    Ajout de l'entitié player (non testé)

---
+++ src/indie/entities/Player.cpp
@@ -0,0 +1,29 @@
+//
+// Player.cpp for bomberman in /home/akkari_a/rendu/IndieStudio/src/indie/entities
+// 
+// Made by Adam Akkari
+// Login   <akkari_a@epitech.net>
+// 
+// Started on  Sun Jun 18 12:15:10 2017 Adam Akkari
+// Last update Sun Jun 18 13:00:30 2017 Adam Akkari
+//
+
+#include "engine.hpp"
+#include "entity.hpp"
+
+using namespace indie::component;
+
+ecs::Entity	indie::entity::createPlayer()
+{
+  auto		&ent = indie::engine::entityManager();
+  ecs::Entity	id;
+
+  id = ent.createEntity();
+  ent.addComponentEmplace<Transform>(id);
+  ent.addComponentEmplace<Renderer3d>(id, "BomberMan.b3d", "");
+  ent.addComponentEmplace<BombInfo>(id, 1, 1, 1, 90);
+  ent.addComponentEmplace<LifeNumber>(id, 1);
+  ent.addComponentEmplace<HP>(id, 1);
+  ent.addComponentEmplace<Speed>(id, 1);
+  ent.addComponentEmplace<Movement>(id, 0.05f);
+}
