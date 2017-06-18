2bafbc64073f4eb5fc538969ef6306966b87eb71
Noam Silvy <noam.silvy@epitech.eu>
2017-06-18 12:32:56 +0200

    add namespace scope to compile

--- src/indie/entities/Box.cpp
+++ src/indie/entities/Box.cpp
@@ -5,7 +5,7 @@
 // Login   <akkari_a@epitech.net>
 //
 // Started on  Fri Jun 16 21:40:38 2017 Adam Akkari
-// Last update Sun Jun 18 12:26:19 2017 Noam Silvy
+// Last update Sun Jun 18 12:31:49 2017 Noam Silvy
 //
 
 #include "irrlicht.h"
@@ -17,7 +17,7 @@ ecs::Entity	indie::entity::createBox()
   ecs::Entity	id;
 
   id = ent.createEntity();
-  ent.addComponentEmplace<Transform>(id);
-  ent.addComponentEmplace<Renderer3d>(id, "gfx/cube1.obj", "gfx/brick.jpg");
+  ent.addComponentEmplace<component::Transform>(id);
+  ent.addComponentEmplace<component::Renderer3d>(id, "gfx/cube1.obj", "gfx/brick.jpg");
   return (id);
 }
