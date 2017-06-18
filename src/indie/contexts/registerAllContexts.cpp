c3114fafc14cdcf516a1a557e12991becbe17cdc
Akram Abd-Ali <akram.abd-ali@epitech.eu>
2017-06-18 20:21:05 +0200

    added contexts to game

--- src/indie/contexts/registerAllContexts.cpp
+++ src/indie/contexts/registerAllContexts.cpp
@@ -5,7 +5,7 @@
 // Login   <silvy_n@epitech.net>
 //
 // Started on  Wed Jun 14 03:59:36 2017 Noam Silvy
-// Last update Sun Jun 18 12:22:34 2017 Noam Silvy
+// Last update Sun Jun 18 20:14:42 2017 akram abd-ali
 //
 
 #include "ecs.hpp"
@@ -20,7 +20,12 @@ void		indie::context::registerAllContexts()
 			{indie::system::COLLISION, true},
 			{indie::system::MESH_RENDERER, true},
 			{indie::system::CAMERA_SYSTEM, true},
-			{indie::system::MAP_GENERATOR, true}};
+			{indie::system::MAP_GENERATOR, true},
+  			{indie::system::BOMB, true},
+			{indie::system::EXPLOSION, true},
+  			{indie::system::MOVEMENT, true},
+  			{indie::system::SYSTIME, true},
+  			{indie::system::COLLISION, true}};
 
   indie::engine::contextManager().registerContext(MAIN_MENU, main_menu);
   indie::engine::contextManager().registerContext(GAME, game);
