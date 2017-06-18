928e6bc10351aadc9db1ab1e820784e6b099c923
Noam Silvy <noam.silvy@epitech.eu>
2017-06-18 13:02:32 +0200

    Clearer event.hpp

--- src/indie/events/event.hpp
+++ src/indie/events/event.hpp
@@ -5,7 +5,7 @@
 // Login   <silvy_n@epitech.net>
 //
 // Started on  Fri Jun 16 21:09:13 2017 Noam Silvy
-// Last update Sun Jun 18 12:26:46 2017 Noam Silvy
+// Last update Sun Jun 18 12:48:46 2017 Noam Silvy
 //
 
 #ifndef EVENT_HPP
@@ -17,20 +17,23 @@ namespace indie
   {
     enum EEvent
       {
+	GO_RIGHT,
+	GO_LEFT,
+	GO_UP,
+	GO_DOWN,
+
 	DROP_BOMB,
 	DETONATE_BOMB,
 	MAP_3D_FROM_2D,
 	SPREAD_EXPLOSION,
+
 	// BOMB_EXPLODED -> event happened
 	// GO_RIGHT -> action requested
+
 	COLLIDED,
 	BOMB_DROPPED,
 	DROP_BOMB_ERR,
-	EXPLOSION_ERR,
-	GO_RIGHT,
-	GO_LEFT,
-	GO_UP,
-	GO_DOWN
+	EXPLOSION_ERR
       };
   }
 }
